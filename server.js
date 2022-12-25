06e2af13ab9bc55782c43518c9ac835b12303707
jeffoh <jeffjunho11@naver.com>
2022-12-25 12:28:21 +0900

    ver2.0

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const express = require('express');
 var cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 30002;
+const port = 3000;
+var request = require('request');
+
+var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+const configuration = new Configuration({
+    apiKey: '<apikey>',
+});
+const openai = new OpenAIApi(configuration);
+  
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,27 +24,60 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
+// 번역기능 부분
+var client_id = '<client ID>';
+var client_secret = '<cliend secret>';
+
+// 프론트 전송 부분
 app.get('/content/:data', function(req, res){
     const { data } = req.params;
+    var TransData = '';
     console.log('요청');
+    console.log(data);
+
+    var query = `${data}`;
+    var options_ko = {
+       url: api_url,
+       form: {'source':'ko', 'target':'en', 'text':query},
+       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
     
-    const configuration = new Configuration({
-        apiKey: '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',
+    request.post(options_ko, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            TransData = JSON.parse(body);
+
+            openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: TransData.message.result.translatedText,
+                temperature: 0.7,
+                max_tokens: 256,
+                top_p: 1,
+                frequency_penalty: 0,
+                presence_penalty: 0,
+            }).then(function(result){
+                var options_en = {
+                    url: api_url,
+                    form: {'source':'en', 'target':'ko', 'text':result.data.choices[0].text},
+                    headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+                }
+                request.post(options_en, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        JSONTrans_en = JSON.parse(body);
+                        res.json({"content":`${JSONTrans_en.message.result.translatedText}`});
+                    }else {
+                        res.status(response.statusCode).end();
+                        console.log('error = ' + response.statusCode);
+                    }
+                })
+            });  
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
     });
-    const openai = new OpenAIApi(configuration);
-      
-    openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: data,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    }).then((result) => {
-        console.log(result.data.choices[0].text);
-        res.json({"content":`${result.data.choices[0].text}`});
-    })
+
+    
 })
 
 
